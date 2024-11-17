0fbcd3152545c6e764d783112cee000bef1fa0ef
eg-garus <eg.garus@gmail.com>
2024-11-18 02:52:11 +0400

    first commit

---
+++ src/main/java/com/example/mainproj/MainProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.mainproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mainproj/MainProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mainproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
