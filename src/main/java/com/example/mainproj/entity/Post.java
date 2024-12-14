61a0bd23632ebc68bbbd4a80d048e4889dcfb0ec
eg-garus <eg.garus@gmail.com>
2024-12-15 03:14:10 +0400

    version 0.1

---
+++ src/main/java/com/example/mainproj/entity/Post.java
@@ -0,0 +1,37 @@
+package com.example.mainproj.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Data
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String caption;
+    private String location;
+    private Integer likes;
+
+    @Column
+    @ElementCollection(targetClass = String.class)
+    private Set<String> linkedUser= new HashSet<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+    @OneToMany(cascade = CascadeType.REFRESH, fetch = FetchType.EAGER, mappedBy = "post",orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @PrePersist
+    protected void onCreate()
+    {
+        this.createdDate = LocalDateTime.now();
+    }
+}
